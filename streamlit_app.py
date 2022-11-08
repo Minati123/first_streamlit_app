5845add5b3cde37fe00afc35ac22187bf056fcde
Minati <117758310+Minati123@users.noreply.github.com>
2022-11-08 22:15:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal')
+streamlit.text('Fruit Salad')
+streamlit.text('Sandwitch')
